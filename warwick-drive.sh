7c855132b4ac4453b4e537c0f64bf973fa9f224e
Tom <tom@tomsteer.me>
2022-10-28 03:06:49 +0100

    Modifications + New Scripts

--- warwick-drive.sh
+++ warwick-drive.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 sudo mount -t davfs -o rw,nosuid https://myfiles.warwick.ac.uk/hcwebdav/h-drive /mnt/h
-sudo chown tom:tom /mnt/h
+sudo chown $(whoami):$(id -ng) /mnt/h
 sudo chmod 770 /mnt/h
 
